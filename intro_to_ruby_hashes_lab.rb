112589b2e4915fa27ce921a591d9e5e21e6f3cd2
Baruch Phillips <baruchphillips@gmail.com>
2019-10-31 20:34:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,7 @@ def monopoly_with_third_tier
       :names => {
         :reading_railroad => {},
         :pennsylvania_railroad => {}, 
-        :alex => {},
+        :b_and_o_railroad => {},
         :sam => {}
       }
   }
