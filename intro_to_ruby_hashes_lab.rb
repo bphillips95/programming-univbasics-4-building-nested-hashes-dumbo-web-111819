bced9b9bf5cddc2032388a7ae8be665cde9c281a
Baruch Phillips <baruchphillips@gmail.com>
2019-10-31 20:29:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,11 +23,12 @@ def monopoly_with_third_tier
     cities = {:railroads => {
     :pieces => 4,
       :rent_in_dollars => {
-        :names => "Jossh",
+        :names => {},
         :main_st => {},
         :kingston_ave => {},
         :chandler_st => {}
       }
+      :names => {}
   }
   }
 end
