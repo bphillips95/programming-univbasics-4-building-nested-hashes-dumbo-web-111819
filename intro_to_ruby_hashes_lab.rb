68a3ea67132c35cca895b8d31dbfe46660742b07
Baruch Phillips <baruchphillips@gmail.com>
2019-10-31 20:22:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,8 +10,8 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   cities = {:railroads => {
-    :pieces => {4}
-   }
+    :pieces => {4
+   
   }
 end
 
