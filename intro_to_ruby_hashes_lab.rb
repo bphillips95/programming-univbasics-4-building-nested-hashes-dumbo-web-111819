2e646a7cd27cd3035355105c22d9dfa9ec6c18c0
Baruch Phillips <baruchphillips@gmail.com>
2019-10-31 20:33:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ def monopoly_with_third_tier
     :pieces => 4,
       :rent_in_dollars => {
         :park_ave => {},
-        :main_st => {},
+        :three_pieces_owned => 100,
         :two_pieces_owned => 50,
         :one_piece_owned => 25
       },
